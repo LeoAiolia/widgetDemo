df1775f880e8891a2bb32584fc525fe34d0537f4
run <a936381813@163.com>
2025-02-07 18:39:25 +0800

    init

---
+++ WidgetDemo/WidgetDemo/WidgetDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetDemoApp.swift
+//  WidgetDemo
+//
+//  Created by run on 2025/2/7.
+//
+
+import SwiftUI
+
+@main
+struct WidgetDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
